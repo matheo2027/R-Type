8ffca0c52519de3ec5f0c47e122a303ff2ab7897
matheo2027 <matheo.piques@epitech.eu>
2024-10-16 07:09:40 +0200

    [FIX] RM all files

--- StopAll.sh
+++ StopAll.sh
@@ -1,5 +1,5 @@
 # Nettoie le cache CMake si nécessaire
-rm -rf CMakeCache.txt CMakeFiles Doxyfile *.cmake CMakeDoxyfile.in docs Makefile
+rm -rf CMakeCache.txt CMakeFiles Doxyfile *.cmake CMakeDoxyfile.in docs Makefile ecs/Makefile ecs/*.cmake ecs/CMakeFiles client/Makefile client/*.cmake client/CMakeFiles
 
 # Suppr le dossier build
 rm -rf build
