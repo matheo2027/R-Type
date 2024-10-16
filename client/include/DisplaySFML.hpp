8611d594d1ebabc1a9f3d408893d4e02b62e2ead
matheo2027 <matheo.piques@epitech.eu>
2024-10-16 05:38:41 +0200

    [FIX] Code formating

--- client/include/DisplaySFML.hpp
+++ client/include/DisplaySFML.hpp
@@ -4,18 +4,18 @@
 
 namespace gfx
 {
-    class DisplaySFML : public IDisplay
-    {
-    public:
-        DisplaySFML(u32 width, u32 height, std::string &title);
+class DisplaySFML : public IDisplay
+{
+public:
+    DisplaySFML(u32 width, u32 height, std::string &title);
 
-        void update() override;
-        void clear() override;
-        bool isRunning() const override;
-        void close() override;
-        void draw(const ITexture &texture, u32 x, u32 y) override;
-        void draw(const std::string &text, u32 x, u32 y) override;
-        bool getKeyDown(u8 key) const override;
-        sf::RenderWindow m_window;
-    };
+    void update() override;
+    void clear() override;
+    bool isRunning() const override;
+    void close() override;
+    void draw(const ITexture &texture, u32 x, u32 y) override;
+    void draw(const std::string &text, u32 x, u32 y) override;
+    bool getKeyDown(u8 key) const override;
+    sf::RenderWindow m_window;
+};
 }
--- client/include/IDisplay.hpp
+++ client/include/IDisplay.hpp
@@ -7,70 +7,79 @@
 
 namespace gfx
 {
-    enum key
+enum key
+{
+    K_UP = 0,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_SPACE,
+    K_ESC,
+    K_A,
+    K_B,
+    K_C,
+    K_D,
+    K_E,
+    K_F,
+    K_G,
+    K_H,
+    K_I,
+    K_J,
+    K_K,
+    K_L,
+    K_M,
+    K_N,
+    K_O,
+    K_P,
+    K_Q,
+    K_R,
+    K_S,
+    K_T,
+    K_U,
+    K_V,
+    K_W,
+    K_X,
+    K_Y,
+    K_Z
+};
+
+class ITexture
+{
+public:
+    ITexture(const std::filesystem::path &path) : m_path(path) {}
+    virtual ~ITexture() = default;
+    const std::filesystem::path &getPath() const
     {
-        K_UP = 0,
-        K_DOWN,
-        K_LEFT,
-        K_RIGHT,
-        K_SPACE,
-        K_ESC,
-        K_A,
-        K_B,
-        K_C,
-        K_D,
-        K_E,
-        K_F,
-        K_G,
-        K_H,
-        K_I,
-        K_J,
-        K_K,
-        K_L,
-        K_M,
-        K_N,
-        K_O,
-        K_P,
-        K_Q,
-        K_R,
-        K_S,
-        K_T,
-        K_U,
-        K_V,
-        K_W,
-        K_X,
-        K_Y,
-        K_Z
-    };
+        return m_path;
+    }
 
-    class ITexture
-        {
-        public:
-            ITexture(const std::filesystem::path &path) : m_path(path) {}
-            virtual ~ITexture() = default;
-            const std::filesystem::path &getPath() const { return m_path; }
+protected:
+    std::filesystem::path m_path;
+};
 
-        protected:
-            std::filesystem::path m_path;
-        };
-
-    class IDisplay
+class IDisplay
+{
+public:
+    IDisplay(u32 width, u32 height, std::string &title) : m_width(width), m_height(height) {}
+    virtual ~IDisplay() = default;
+    virtual void update() = 0;
+    virtual void clear() = 0;
+    virtual bool isRunning() const = 0;
+    virtual void close() = 0;
+    virtual void draw(const ITexture &texture, u32 x, u32 y) = 0;
+    virtual void draw(const std::string &text, u32 x, u32 y) = 0;
+    virtual bool getKeyDown(u8 key) const = 0;
+    u32 getWidth() const
     {
-        public:
-            IDisplay(u32 width, u32 height, std::string &title) : m_width(width), m_height(height) {}
-            virtual ~IDisplay() = default;
-            virtual void update() = 0;
-            virtual void clear() = 0;
-            virtual bool isRunning() const = 0;
-            virtual void close() = 0;
-            virtual void draw(const ITexture &texture, u32 x, u32 y) = 0;
-            virtual void draw(const std::string &text, u32 x, u32 y) = 0;
-            virtual bool getKeyDown(u8 key) const = 0;
-            u32 getWidth() const { return m_width; }
-            u32 getHeight() const { return m_height; }
+        return m_width;
+    }
+    u32 getHeight() const
+    {
+        return m_height;
+    }
 
-        protected:
-            u32 m_width;
-            u32 m_height;
-    };
+protected:
+    u32 m_width;
+    u32 m_height;
+};
 }
