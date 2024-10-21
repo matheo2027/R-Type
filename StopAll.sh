58deda748007e0e878f493342ae07d9223081582
matheo2027 <matheo.piques@epitech.eu>
2024-10-21 10:22:16 +0200

    [FIX] StopAll.sh, add line for rm doc

--- StopAll.sh
+++ StopAll.sh
@@ -1,5 +1,8 @@
 # Nettoie le cache CMake si nécessaire
-rm -rf CMakeCache.txt CMakeFiles Doxyfile *.cmake CMakeDoxyfile.in docs Makefile ecs/Makefile ecs/*.cmake ecs/CMakeFiles client/Makefile client/*.cmake client/CMakeFiles
+rm -rf CMakeCache.txt CMakeFiles Doxyfile *.cmake CMakeDoxyfile.in Makefile ecs/Makefile ecs/*.cmake ecs/CMakeFiles client/Makefile client/*.cmake client/CMakeFiles
+
+# # Suppr le dossier docs
+# rm -rf docs
 
 # Suppr le dossier build
 rm -rf build
