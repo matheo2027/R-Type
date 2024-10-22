0c73314181adaee6328098de28e3646baddbc858
matheo2027 <matheo.piques@epitech.eu>
2024-10-22 16:23:14 +0200

    [FIX] bug in stopall.sh

--- StopAll.sh
+++ StopAll.sh
@@ -1,5 +1,5 @@
 # Nettoie le cache CMake si nécessaire
-rm -rf CMakeCache.txt CMakeFiles Doxyfile *.cmake CMakeDoxyfile.in Makefile ecs/Makefile ecs/*.cmake ecs/CMakeFiles client/Makefile client/*.cmake client/CMakeFiles
+rm -rf CMakeCache.txt CMakeFiles Doxyfile *.cmake CMakeDoxyfile.in Makefile ecs/Makefile ecs/*.cmake ecs/CMakeFiles client/Makefile client/*.cmake client/CMakeFiles server/Makefile server/*.cmake server/CMakeFiles
 
 # Suppr le dossier build
 rm -rf build
