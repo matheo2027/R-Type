1aedff4cac00a05cabacb762850b4610ef7c124b
matheo2027 <matheo.piques@epitech.eu>
2024-10-30 20:24:19 +0100

    [UPD] ADD doc Render.hpp

--- ecs/include/systems/Render.hpp
+++ ecs/include/systems/Render.hpp
@@ -1,3 +1,11 @@
+/**
+ * @file Render.hpp
+ * @brief Declaration of the Render class for rendering entities in the ECS.
+ *
+ * This class is responsible for rendering entities to the display using
+ * an entity-manager system. It implements the ISystem interface.
+ */
+
 #pragma once
 
 #include <vector>
@@ -10,16 +18,34 @@
 namespace systems
 {
 
+/**
+ * @class Render
+ * @brief Handles the rendering of entities in the game.
+ *
+ * The Render class is part of the ECS (Entity-Component-System) architecture.
+ * It is responsible for updating and rendering entities to the display each frame.
+ */
 class Render : public ISystem
 {
     public:
+        /**
+         * @brief Constructs a Render object.
+         * @param entityManager Reference to the EntityManager that manages the entities.
+         * @param display Reference to the display interface used for rendering.
+         */
         Render(ecs::EntityManager &entityManager, display::IDisplay &display);
 
+        /**
+         * @brief Updates the rendering system.
+         *
+         * This method is called every frame to update the rendering of entities.
+         * @param dt The time elapsed since the last update, in seconds.
+         */
         void update(float dt);
 
     private:
-        ecs::EntityManager &m_entityManager;
-        display::IDisplay &m_display;
+        ecs::EntityManager &m_entityManager; ///< Reference to the EntityManager.
+        display::IDisplay &m_display;         ///< Reference to the display interface.
 };
 
 }
