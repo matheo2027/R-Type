6ccdfaa5ccf78ea7fbe58d16fdd615f137bb240d
matheo2027 <matheo.piques@epitech.eu>
2024-10-30 20:25:45 +0100

    [UPD] ADD doc EntityManager.hpp

--- ecs/include/EntityManager.hpp
+++ ecs/include/EntityManager.hpp
@@ -1,3 +1,8 @@
+/**
+ * @file EntityManager.hpp
+ * @brief Manages entities and their components in an Entity-Component-System (ECS) architecture.
+ */
+
 #pragma once
 
 #include <vector>
@@ -18,57 +23,78 @@
 
 #include <iostream>
 
-namespace ecs
-{
+namespace ecs {
 
+/**
+ * @class EntityManager
+ * @brief Class responsible for managing entities and their associated components.
+ *
+ * The EntityManager handles the creation of entities and their components,
+ * allowing for dynamic addition, retrieval, and removal of components associated
+ * with each entity. This is part of the Entity-Component-System (ECS) pattern,
+ * which promotes a separation of data and behavior.
+ */
+class EntityManager {
+public:
+    /**
+     * @brief Constructs an EntityManager instance.
+     */
+    EntityManager();
 
-class EntityManager
-{
-    public:
-        EntityManager();
+    /**
+     * @brief Adds a component to an entity.
+     *
+     * This method creates a new component of type T for the specified entity.
+     *
+     * @tparam T The type of the component to be added.
+     * @tparam Args Types of the constructor arguments for the component.
+     * @param entity The entity to which the component will be added.
+     * @param args Arguments to initialize the component.
+     */
+    template <typename T, typename... Args>
+    void addComponent(Entity entity, Args&&... args);
 
-        template <typename T, typename... Args>
-        void addComponent(Entity entity, Args&&... args)
-        {
-            auto &components = m_components[typeid(T)];
-            if (components.size() <= entity) {
-                components.resize(entity + 1);
-                m_size = entity + 1;
-            }
-            components[entity] = T(std::forward<Args>(args)...);
-        }
+    /**
+     * @brief Retrieves a component of a specific type from an entity.
+     *
+     * This method returns a pointer to the component of type T associated with
+     * the specified entity.
+     *
+     * @tparam T The type of the component to retrieve.
+     * @param entity The entity from which to retrieve the component.
+     * @return A pointer to the component of type T, or nullptr if not found.
+     */
+    template <typename T>
+    T* getComponent(Entity entity);
 
-        template <typename T>
-        T* getComponent(Entity entity)
-        {
-            auto it = m_components.find(typeid(T));
-            if (it != m_components.end() && it->second.size() > entity) {
-                return std::any_cast<T>(&it->second[entity]);
-            }
-            return nullptr;
-        }
+    /**
+     * @brief Removes an entity and its associated components.
+     *
+     * This method resets all components associated with the specified entity.
+     *
+     * @param entity The entity to remove.
+     */
+    void removeEntity(Entity entity);
 
-        void removeEntity(Entity entity)
-        {
-            for (auto &[type, components] : m_components) {
-                if (components.size() > entity) {
-                    components[entity].reset();
-                }
-            }
-        }
+    /**
+     * @brief Creates a new entity.
+     *
+     * This method generates a new entity and returns its ID.
+     *
+     * @return The ID of the newly created entity.
+     */
+    Entity createEntity();
 
-        Entity createEntity()
-        {
-            return m_size;
-        }
+    /**
+     * @brief Gets the number of entities managed by the EntityManager.
+     *
+     * @return The number of entities.
+     */
+    unsigned int size() const;
 
-        unsigned int size() const { return m_size; }
-
-    private:
-        std::unordered_map<std::type_index, std::vector<std::any>> m_components;
-        int m_size = 0;
-
+private:
+    std::unordered_map<std::type_index, std::vector<std::any>> m_components; ///< Map of component types to their instances.
+    int m_size = 0; ///< The current number of entities managed.
 };
 
-}
-
+} // namespace ecs
