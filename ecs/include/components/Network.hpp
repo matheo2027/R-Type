4d12071d7634329388db4e27052c49e75c73f931
matheo2027 <matheo.piques@epitech.eu>
2024-10-30 20:13:44 +0100

    [UPD] ADD doc Network.hpp

--- ecs/include/components/Network.hpp
+++ ecs/include/components/Network.hpp
@@ -1,3 +1,8 @@
+/**
+ * @file NetworkComponent.hpp
+ * @brief Contains the definition of the Network component.
+ */
+
 #ifndef NETWORKCOMPONENT_HPP
 #define NETWORKCOMPONENT_HPP
 
@@ -5,9 +10,21 @@
 
 namespace component {
 
+/**
+ * @struct Network
+ * @brief Represents a network component in the Entity-Component-System (ECS) architecture.
+ *
+ * The Network component holds the network ID associated with an entity.
+ * It is used to identify the entity in network communications.
+ */
 struct Network : public Component {
+    /// The unique identifier for the network.
     int networkId;
 
+    /**
+     * @brief Constructs a Network component.
+     * @param networkId The unique network identifier (default is 0).
+     */
     Network(int networkId = 0) : Component(), networkId(networkId) {}
 };
 
