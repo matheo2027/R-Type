09ee335425c056cffde8a051d3e7e2466cdd37f7
matheo2027 <matheo.piques@epitech.eu>
2024-10-30 20:20:20 +0100

    [UPD] ADD doc Box.hpp

--- ecs/include/systems/Box.hpp
+++ ecs/include/systems/Box.hpp
@@ -6,15 +6,34 @@
 namespace systems
 {
 
+/**
+ * @class Box
+ * @brief This class represents a system that manages box-related entities.
+ *
+ * The Box class is derived from the ISystem interface and is responsible for
+ * updating box entities within the ECS (Entity-Component-System) architecture.
+ */
 class Box : public ISystem
 {
-    public:
-        Box(ecs::EntityManager &entityManager);
+public:
+    /**
+     * @brief Constructs a Box system.
+     *
+     * @param entityManager Reference to the EntityManager for managing entities.
+     */
+    Box(ecs::EntityManager &entityManager);
 
-        void update(float dt) override;
+    /**
+     * @brief Updates the Box system.
+     *
+     * This method is called to update the state of the box entities.
+     *
+     * @param dt The time elapsed since the last update (in seconds).
+     */
+    void update(float dt) override;
 
-    private:
-        ecs::EntityManager &m_entityManager;
+private:
+    ecs::EntityManager &m_entityManager; ///< Reference to the EntityManager instance.
 };
 
-}
+} // namespace systems
