db75fcd09b8da157db64093459c461edc459bd85
matheo2027 <matheo.piques@epitech.eu>
2024-10-30 20:08:02 +0100

    [UPD] ADD doc Box.hpp

--- ecs/include/components/Box.hpp
+++ ecs/include/components/Box.hpp
@@ -1,3 +1,12 @@
+/**
+ * @file Box.hpp
+ * @brief Defines the Box component for collision detection.
+ *
+ * This file contains the definition of the Box structure, which represents a
+ * rectangular area in a 2D space. The Box structure includes methods for
+ * testing collisions with other Box instances.
+ */
+
 #ifndef BOX_HPP
 #define BOX_HPP
 
@@ -5,14 +14,38 @@
 
 namespace component {
 
+/**
+ * @struct Box
+ * @brief Represents a rectangular area defined by its position and dimensions.
+ *
+ * The Box structure is derived from the Component class and contains
+ * properties for the position and size of the rectangle.
+ */
 struct Box : public Component {
-    int x;
-    int y;
-    int width;
-    int height;
+    int x;      ///< The x-coordinate of the top-left corner of the box.
+    int y;      ///< The y-coordinate of the top-left corner of the box.
+    int width;  ///< The width of the box.
+    int height; ///< The height of the box.
 
+    /**
+     * @brief Constructs a Box instance.
+     *
+     * @param x The x-coordinate of the top-left corner of the box.
+     * @param y The y-coordinate of the top-left corner of the box.
+     * @param width The width of the box.
+     * @param height The height of the box.
+     */
     Box(int x, int y, int width, int height) : x(x), y(y), width(width), height(height) {}
 
+    /**
+     * @brief Tests for collision with another Box.
+     *
+     * This method checks if the current Box instance collides with
+     * another Box instance.
+     *
+     * @param other The other Box instance to test for collision.
+     * @return true if the boxes collide, false otherwise.
+     */
     bool testCollision(const Box &other) const {
         return x < other.x + other.width &&
                x + width > other.x &&
@@ -21,6 +54,6 @@ struct Box : public Component {
     }
 };
 
-}
+} // namespace component
 
 #endif // BOX_HPP
