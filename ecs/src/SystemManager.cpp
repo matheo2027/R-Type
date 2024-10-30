ff47fa59d91023ec06197570400f9f05075d6b14
matheo2027 <matheo.piques@epitech.eu>
2024-10-30 20:33:56 +0100

    [UPD] ADD doc SystemManager.cpp

--- ecs/src/SystemManager.cpp
+++ ecs/src/SystemManager.cpp
@@ -1,17 +1,50 @@
-#include "SystemManager.hpp"
+/**
+ * @file SystemManager.hpp
+ * @brief This file defines the SystemManager class for managing ECS (Entity-Component-System) systems.
+ */
+
+#ifndef SYSTEMMANAGER_HPP
+#define SYSTEMMANAGER_HPP
+
+#include <vector>
+#include <memory>
+#include "System.hpp"
 
 namespace ecs
 {
 
-SystemManager::SystemManager()
+/**
+ * @class SystemManager
+ * @brief Manages a collection of systems in the ECS architecture.
+ *
+ * The SystemManager is responsible for updating all systems in the ECS framework
+ * at each frame. It iterates through the list of systems and calls their update
+ * methods with the time delta.
+ */
+class SystemManager
 {
-}
+public:
+    /**
+     * @brief Constructs a new SystemManager object.
+     *
+     * Initializes an empty SystemManager.
+     */
+    SystemManager();
 
-void SystemManager::update(float dt)
-{
-    for (auto &system : systems) {
-        system->update(dt);
-    }
-}
+    /**
+     * @brief Updates all registered systems.
+     *
+     * Calls the update method on each system in the systems vector.
+     *
+     * @param dt The time delta since the last update, used to synchronize system updates.
+     */
+    void update(float dt);
 
-}
+private:
+    /// A vector of unique pointers to the systems managed by this SystemManager.
+    std::vector<std::unique_ptr<System>> systems;
+};
+
+} // namespace ecs
+
+#endif // SYSTEMMANAGER_HPP
