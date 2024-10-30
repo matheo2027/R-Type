5057a6945e326a4e8d5f392048d5d00eeebcc6b6
matheo2027 <matheo.piques@epitech.eu>
2024-10-30 20:42:37 +0100

    [FIX] fix doc and compil EntityManager.hpp  main

--- ecs/include/EntityManager.hpp
+++ ecs/include/EntityManager.hpp
@@ -1,6 +1,9 @@
 /**
  * @file EntityManager.hpp
- * @brief Manages entities and their components in an Entity-Component-System (ECS) architecture.
+ * @brief This file contains the definition of the EntityManager class, which manages components for entities in an ECS (Entity-Component-System) architecture.
+ *
+ * The EntityManager class allows for adding, retrieving, and removing components associated with entities.
+ * It uses type indexing to store components and provides methods to manage entities efficiently.
  */
 
 #pragma once
@@ -23,78 +26,77 @@
 
 #include <iostream>
 
-namespace ecs {
+namespace ecs
+{
 
 /**
  * @class EntityManager
- * @brief Class responsible for managing entities and their associated components.
+ * @brief Manages the components for entities in an ECS architecture.
  *
- * The EntityManager handles the creation of entities and their components,
- * allowing for dynamic addition, retrieval, and removal of components associated
- * with each entity. This is part of the Entity-Component-System (ECS) pattern,
- * which promotes a separation of data and behavior.
+ * The EntityManager allows for the addition, retrieval, and removal of components associated with entities.
+ * It handles the storage of components using type indexing and ensures efficient memory management.
  */
-class EntityManager {
-public:
-    /**
-     * @brief Constructs an EntityManager instance.
-     */
-    EntityManager();
+class EntityManager
+{
+    public:
+        /**
+         * @brief Constructs an EntityManager.
+         */
+        EntityManager();
 
-    /**
-     * @brief Adds a component to an entity.
-     *
-     * This method creates a new component of type T for the specified entity.
-     *
-     * @tparam T The type of the component to be added.
-     * @tparam Args Types of the constructor arguments for the component.
-     * @param entity The entity to which the component will be added.
-     * @param args Arguments to initialize the component.
-     */
-    template <typename T, typename... Args>
-    void addComponent(Entity entity, Args&&... args);
+        /**
+         * @brief Adds a component of type T to the specified entity.
+         *
+         * This method resizes the internal storage for components if necessary and constructs a new component using the provided arguments.
+         *
+         * @tparam T The type of the component to add.
+         * @tparam Args The types of the arguments used to construct the component.
+         * @param entity The entity to which the component will be added.
+         * @param args Arguments used to construct the component of type T.
+         */
+        template <typename T, typename... Args>
+        void addComponent(Entity entity, Args&&... args);
 
-    /**
-     * @brief Retrieves a component of a specific type from an entity.
-     *
-     * This method returns a pointer to the component of type T associated with
-     * the specified entity.
-     *
-     * @tparam T The type of the component to retrieve.
-     * @param entity The entity from which to retrieve the component.
-     * @return A pointer to the component of type T, or nullptr if not found.
-     */
-    template <typename T>
-    T* getComponent(Entity entity);
+        /**
+         * @brief Retrieves a component of type T associated with the specified entity.
+         *
+         * This method returns a pointer to the component if it exists, or nullptr if it does not.
+         *
+         * @tparam T The type of the component to retrieve.
+         * @param entity The entity from which the component will be retrieved.
+         * @return A pointer to the component of type T, or nullptr if it does not exist.
+         */
+        template <typename T>
+        T* getComponent(Entity entity);
 
-    /**
-     * @brief Removes an entity and its associated components.
-     *
-     * This method resets all components associated with the specified entity.
-     *
-     * @param entity The entity to remove.
-     */
-    void removeEntity(Entity entity);
+        /**
+         * @brief Removes all components associated with the specified entity.
+         *
+         * This method resets all components for the given entity, effectively removing it from the system.
+         *
+         * @param entity The entity to remove.
+         */
+        void removeEntity(Entity entity);
 
-    /**
-     * @brief Creates a new entity.
-     *
-     * This method generates a new entity and returns its ID.
-     *
-     * @return The ID of the newly created entity.
-     */
-    Entity createEntity();
+        /**
+         * @brief Creates a new entity and returns its ID.
+         *
+         * The ID is unique and incremented for each new entity created.
+         *
+         * @return The ID of the newly created entity.
+         */
+        Entity createEntity();
 
-    /**
-     * @brief Gets the number of entities managed by the EntityManager.
-     *
-     * @return The number of entities.
-     */
-    unsigned int size() const;
+        /**
+         * @brief Returns the total number of entities managed by the EntityManager.
+         *
+         * @return The number of entities.
+         */
+        unsigned int size() const;
 
-private:
-    std::unordered_map<std::type_index, std::vector<std::any>> m_components; ///< Map of component types to their instances.
-    int m_size = 0; ///< The current number of entities managed.
+    private:
+        std::unordered_map<std::type_index, std::vector<std::any>> m_components; ///< Map of components indexed by type.
+        int m_size = 0; ///< The total number of entities managed.
 };
 
 } // namespace ecs
