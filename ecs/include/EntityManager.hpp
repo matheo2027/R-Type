a5b7f77ed85e996adef31379ae62928ab9e3b9de
matheo2027 <matheo.piques@epitech.eu>
2024-10-30 20:51:34 +0100

    [FIX] fix doc and compil EntityManager.hpp

--- ecs/include/EntityManager.hpp
+++ ecs/include/EntityManager.hpp
@@ -21,53 +21,93 @@
 namespace ecs
 {
 
-
+/**
+ * @class EntityManager
+ * @brief Manages entities and their components in an ECS (Entity-Component-System) architecture.
+ *
+ * The EntityManager is responsible for creating entities and managing their associated components.
+ * It allows the addition, retrieval, and removal of components associated with entities.
+ */
 class EntityManager
 {
-    public:
-        EntityManager();
+public:
+    /**
+     * @brief Constructs an EntityManager object.
+     */
+    EntityManager();
 
-        template <typename T, typename... Args>
-        void addComponent(Entity entity, Args&&... args)
-        {
-            auto &components = m_components[typeid(T)];
-            if (components.size() <= entity) {
-                components.resize(entity + 1);
-                m_size = entity + 1;
+    /**
+     * @brief Adds a component of type T to the specified entity.
+     *
+     * @tparam T The type of component to add.
+     * @tparam Args The types of the arguments to construct the component.
+     * @param entity The identifier of the entity to which the component will be added.
+     * @param args The arguments to construct the component of type T.
+     */
+    template <typename T, typename... Args>
+    void addComponent(Entity entity, Args&&... args)
+    {
+        auto &components = m_components[typeid(T)];
+        if (components.size() <= entity) {
+            components.resize(entity + 1);
+            m_size = entity + 1;
+        }
+        components[entity] = T(std::forward<Args>(args)...);
+    }
+
+    /**
+     * @brief Retrieves a pointer to the component of type T associated with the specified entity.
+     *
+     * @tparam T The type of the component to retrieve.
+     * @param entity The identifier of the entity from which to retrieve the component.
+     * @return A pointer to the component of type T if it exists, otherwise nullptr.
+     */
+    template <typename T>
+    T* getComponent(Entity entity)
+    {
+        auto it = m_components.find(typeid(T));
+        if (it != m_components.end() && it->second.size() > entity) {
+            return std::any_cast<T>(&it->second[entity]);
+        }
+        return nullptr;
+    }
+
+    /**
+     * @brief Removes all components associated with the specified entity.
+     *
+     * This function resets all components of the entity, effectively removing it from the EntityManager.
+     *
+     * @param entity The identifier of the entity to remove.
+     */
+    void removeEntity(Entity entity)
+    {
+        for (auto &[type, components] : m_components) {
+            if (components.size() > entity) {
+                components[entity].reset();
             }
-            components[entity] = T(std::forward<Args>(args)...);
         }
+    }
 
-        template <typename T>
-        T* getComponent(Entity entity)
-        {
-            auto it = m_components.find(typeid(T));
-            if (it != m_components.end() && it->second.size() > entity) {
-                return std::any_cast<T>(&it->second[entity]);
-            }
-            return nullptr;
-        }
+    /**
+     * @brief Creates a new entity and returns its identifier.
+     *
+     * @return The identifier of the newly created entity.
+     */
+    Entity createEntity()
+    {
+        return m_size;
+    }
 
-        void removeEntity(Entity entity)
-        {
-            for (auto &[type, components] : m_components) {
-                if (components.size() > entity) {
-                    components[entity].reset();
-                }
-            }
-        }
+    /**
+     * @brief Retrieves the total number of entities managed by the EntityManager.
+     *
+     * @return The number of entities.
+     */
+    unsigned int size() const { return m_size; }
 
-        Entity createEntity()
-        {
-            return m_size;
-        }
-
-        unsigned int size() const { return m_size; }
-
-    private:
-        std::unordered_map<std::type_index, std::vector<std::any>> m_components;
-        int m_size = 0;
-
+private:
+    std::unordered_map<std::type_index, std::vector<std::any>> m_components; /**< A map storing vectors of components indexed by their type. */
+    int m_size = 0; /**< The current number of entities managed by the EntityManager. */
 };
 
 }
