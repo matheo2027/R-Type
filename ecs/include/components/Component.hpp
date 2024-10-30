1cfaa9847a91e45c49099ef9163d5d189d6a1f85
matheo2027 <matheo.piques@epitech.eu>
2024-10-30 20:10:38 +0100

    [UPD] ADD doc Component.hpp

--- ecs/include/components/Component.hpp
+++ ecs/include/components/Component.hpp
@@ -1,11 +1,31 @@
+/**
+ * @file Component.hpp
+ * @brief Header file for the Component structure.
+ *
+ * This file defines the base class for all components in the ECS (Entity-Component-System) architecture.
+ */
+
 #ifndef COMPONENT_HPP
 #define COMPONENT_HPP
 
 namespace component
 {
 
+/**
+ * @struct Component
+ * @brief Base class for all components in the ECS.
+ *
+ * The Component struct serves as a base class for all components used in the ECS architecture.
+ * It provides a virtual destructor to ensure proper cleanup of derived classes.
+ * All components should inherit from this struct to be managed by the ECS.
+ */
 struct Component
 {
+    /**
+     * @brief Virtual destructor for the Component struct.
+     *
+     * The virtual destructor allows for proper cleanup of derived classes.
+     */
     virtual ~Component() = default;
 };
 
