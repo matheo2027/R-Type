88512bff953a092a609319fbd81e5a7db0e89510
matheo2027 <matheo.piques@epitech.eu>
2024-10-30 20:18:34 +0100

    [UPD] ADD doc IDisplay.hpp

--- ecs/include/display/IDisplay.hpp
+++ ecs/include/display/IDisplay.hpp
@@ -1,3 +1,11 @@
+/**
+ * @file IDisplay.hpp
+ * @brief Interface for display management in a game.
+ *
+ * This file contains the definition of the IDisplay interface and the ITexture structure,
+ * which are used for handling textures and display operations in a graphical application.
+ */
+
 #pragma once
 
 #include <string>
@@ -6,43 +14,127 @@
 namespace display
 {
 
+/**
+ * @enum Key
+ * @brief Enumeration for key input representation.
+ *
+ * This enumeration defines the keys that can be checked for input in the application.
+ */
 enum Key
 {
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
-    K_SPACE,
+    K_UP,    ///< Represents the up arrow key.
+    K_DOWN,  ///< Represents the down arrow key.
+    K_LEFT,  ///< Represents the left arrow key.
+    K_RIGHT, ///< Represents the right arrow key.
+    K_SPACE, ///< Represents the spacebar key.
 };
 
+/**
+ * @struct ITexture
+ * @brief Represents a texture in the application.
+ *
+ * The ITexture structure is used to store texture information and provide access
+ * to the texture's file path.
+ */
 struct ITexture
 {
+    /**
+     * @brief Constructor for ITexture.
+     * @param path The file path of the texture.
+     */
     ITexture(const std::string &path) : m_path(path) {}
+
+    /**
+     * @brief Destructor for ITexture.
+     */
     virtual ~ITexture() = default;
 
+    /**
+     * @brief Get the path of the texture.
+     * @return The file path of the texture.
+     */
     const std::string &getPath() const { return m_path; }
-    
-    std::string m_path;
+
+    std::string m_path; ///< The file path of the texture.
 };
 
+/**
+ * @class IDisplay
+ * @brief Interface for managing the display and rendering in a graphical application.
+ *
+ * The IDisplay interface defines the methods required for initializing, updating,
+ * drawing, and handling input for a graphical display.
+ */
 class IDisplay
 {
     public:
+        /**
+         * @brief Destructor for IDisplay.
+         */
         virtual ~IDisplay() = default;
-        
+
+        /**
+         * @brief Initialize the display.
+         * @param width The width of the display.
+         * @param height The height of the display.
+         * @param title The title of the window.
+         */
         virtual void init(int width, int height, const std::string &title) = 0;
 
+        /**
+         * @brief Update the display.
+         *
+         * This method is called to update the display for rendering.
+         */
         virtual void update() = 0;
+
+        /**
+         * @brief Clear the display.
+         *
+         * This method is called to clear the display for the next frame.
+         */
         virtual void clear() = 0;
 
+        /**
+         * @brief Check if the display is open.
+         * @return True if the display is open; otherwise, false.
+         */
         virtual bool isOpen() const = 0;
+
+        /**
+         * @brief Close the display.
+         *
+         * This method is called to close the display window.
+         */
         virtual void close() = 0;
 
+        /**
+         * @brief Draw a texture on the display.
+         * @param texture A shared pointer to the texture to draw.
+         * @param x The x-coordinate where the texture should be drawn.
+         * @param y The y-coordinate where the texture should be drawn.
+         */
         virtual void draw(std::shared_ptr<ITexture> &texture, float x, float y) = 0;
 
+        /**
+         * @brief Check if a key is currently pressed.
+         * @param key The key to check.
+         * @return True if the key is pressed; otherwise, false.
+         */
         virtual bool isKeyPressed(Key key) const = 0;
+
+        /**
+         * @brief Check if a key was released.
+         * @param key The key to check.
+         * @return True if the key was released; otherwise, false.
+         */
         virtual bool isKeyReleased(Key key) const = 0;
 
+        /**
+         * @brief Create a texture from a file path.
+         * @param path The file path of the texture.
+         * @return A shared pointer to the created texture.
+         */
         virtual std::shared_ptr<ITexture> createTexture(const std::string &path) = 0;
 };
 
