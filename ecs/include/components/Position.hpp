249507360d529cdc58f12be9c40a576d248ec226
matheo2027 <matheo.piques@epitech.eu>
2024-10-30 20:15:26 +0100

    [UPD] ADD doc Position.hpp

--- ecs/include/components/Position.hpp
+++ ecs/include/components/Position.hpp
@@ -5,13 +5,30 @@
 
 namespace component {
 
+/**
+ * @brief Represents a position in a 2D space.
+ *
+ * The Position struct inherits from the Component class and stores
+ * the x and y coordinates of an entity in the game.
+ * This component can be used to define the position of game entities
+ * in a 2D environment.
+ */
 struct Position : public Component {
+    /// The x-coordinate of the position.
     float x;
+
+    /// The y-coordinate of the position.
     float y;
 
+    /**
+     * @brief Constructs a Position object with the specified coordinates.
+     *
+     * @param x The x-coordinate (default is 0).
+     * @param y The y-coordinate (default is 0).
+     */
     Position(float x = 0, float y = 0) : Component(), x(x), y(y) {}
 };
 
-}
+} // namespace component
 
 #endif // POSITIONCOMPONENT_HPP
