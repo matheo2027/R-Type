fa085a418d0f5c5a34dd067f39d036cdb6aeb83f
matheo2027 <matheo.piques@epitech.eu>
2024-10-30 20:09:46 +0100

    [UPD] ADD doc Bullet.hpp

--- ecs/include/components/Bullet.hpp
+++ ecs/include/components/Bullet.hpp
@@ -1,3 +1,11 @@
+/**
+ * @file Bullet.hpp
+ * @brief Declaration of the Bullet component.
+ *
+ * This file defines the Bullet component, which is used to represent a bullet in the game.
+ * It contains the properties of a bullet, such as speed and angle.
+ */
+
 #ifndef BULLET_HPP
 #define BULLET_HPP
 
@@ -5,16 +13,30 @@
 
 namespace component {
 
+/**
+ * @struct Bullet
+ * @brief Represents a bullet in the game.
+ *
+ * The Bullet struct inherits from the Component class and contains information about
+ * the bullet's speed and angle.
+ */
 struct Bullet : public Component {
-    float speed;
-    float angle;
+    /** The speed of the bullet. */
+    float speed; /**< Speed of the bullet in units per second. */
+    float angle; /**< Angle at which the bullet is fired in degrees. */
 
+    /**
+     * @brief Constructs a Bullet component.
+     *
+     * @param speed The speed of the bullet.
+     * @param angle The angle at which the bullet is fired.
+     */
     Bullet(float speed, float angle)
         : speed(speed), angle(angle)
     {
     }
 };
 
-}
+} // namespace component
 
 #endif // BULLET_HPP
