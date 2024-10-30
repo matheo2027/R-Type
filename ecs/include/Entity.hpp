a39ba7020a8dfbae3f922e6a99cca9a77858e1c9
matheo2027 <matheo.piques@epitech.eu>
2024-10-30 20:25:05 +0100

    [UPD] ADD doc Entity.hpp

--- ecs/include/Entity.hpp
+++ ecs/include/Entity.hpp
@@ -1,3 +1,18 @@
+/**
+ * @file ecs.hpp
+ * @brief Header file defining the Entity type in the ECS (Entity-Component-System) architecture.
+ *
+ * This file contains the definition of the Entity type used in the ECS framework.
+ * An Entity represents a unique identifier for game objects within the ECS.
+ *
+ * @namespace ecs
+ * The ecs namespace contains all the definitions related to the Entity-Component-System architecture.
+ *
+ * @typedef Entity
+ * A type representing an entity in the ECS framework. It is defined as an unsigned integer,
+ * allowing for a wide range of unique identifiers.
+ */
+
 #pragma once
 
 namespace ecs
