e522db9d619e4c1933b4b2ae381f5f53ae1e7d8f
matheo2027 <matheo.piques@epitech.eu>
2024-10-30 20:14:53 +0100

    [UPD] ADD doc Player.hpp

--- ecs/include/components/Player.hpp
+++ ecs/include/components/Player.hpp
@@ -1,3 +1,8 @@
+/**
+ * @file Player.hpp
+ * @brief Declaration of the Player component.
+ */
+
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
 
@@ -5,24 +10,51 @@
 
 namespace component {
 
+/**
+ * @struct Player
+ * @brief Represents the player character in the game.
+ *
+ * This structure inherits from the Component class and contains
+ * attributes that define the player's movement, shooting speed,
+ * and cooldown for shooting actions.
+ */
 struct Player : public Component {
+    /// The speed at which the player moves.
     float speed = 100.0f;
 
+    /// Boolean flag for moving up.
     bool up;
+
+    /// Boolean flag for moving down.
     bool down;
+
+    /// Boolean flag for moving left.
     bool left;
+
+    /// Boolean flag for moving right.
     bool right;
+
+    /// Boolean flag for shooting.
     bool shoot;
 
+    /// The speed at which the player can shoot.
     float shootingSpeed = 0.5f;
+
+    /// The cooldown time before the player can shoot again.
     float shootingCooldown = 0.0f;
 
+    /**
+     * @brief Constructs a Player object with specified speed and shooting speed.
+     *
+     * @param speed The movement speed of the player (default is 100.0f).
+     * @param shootingSpeed The speed at which the player can shoot (default is 0.5f).
+     */
     Player(float speed = 100.0f, float shootingSpeed = 0.5f)
         : speed(speed), shootingSpeed(shootingSpeed)
     {
     }
 };
 
-}
+} // namespace component
 
 #endif // PLAYER_HPP
