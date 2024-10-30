2462938c22ea3401ed3fe1ea3d45d440424a6943
matheo2027 <matheo.piques@epitech.eu>
2024-10-30 20:36:40 +0100

    [UPD] ADD doc Server.hpp

--- server/include/Server.hpp
+++ server/include/Server.hpp
@@ -3,16 +3,38 @@
 
 #include "IServer.hpp"
 
+/**
+ * @class Server
+ * @brief Implements the IServer interface for handling UDP communication.
+ *
+ * The Server class manages the UDP socket for sending and receiving messages.
+ */
 class Server : public IServer
 {
 public:
+    /**
+     * @brief Constructs a Server instance with the specified port.
+     * @param port The port number to bind the server socket.
+     */
     Server(unsigned short port);
+
+    /**
+     * @brief Sends a message to the specified recipient.
+     * @param message A vector of characters representing the message to be sent.
+     * @param recipient The IP address of the recipient.
+     * @param port The port number of the recipient.
+     */
     void send(const std::vector<char>& message, const sf::IpAddress& recipient, unsigned short port) override;
+
+    /**
+     * @brief Receives incoming messages.
+     * This method listens for incoming messages on the bound socket and processes them accordingly.
+     */
     void receive() override;
 
 private:
-    sf::UdpSocket socket;
-    unsigned short port;
+    sf::UdpSocket socket; ///< The UDP socket used for communication.
+    unsigned short port;   ///< The port number used for the server.
 };
 
 #endif // SERVER_HPP
