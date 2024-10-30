9e08fa4c38221710d3e676d07448660cd04b7c63
matheo2027 <matheo.piques@epitech.eu>
2024-10-30 20:05:11 +0100

    [UPD] ADD doc Client.cpp

--- client/src/Client.cpp
+++ client/src/Client.cpp
@@ -1,23 +1,59 @@
+/**
+ * @file Client.cpp
+ * @brief Implementation of the Client class for network communication.
+ *
+ * This file contains the implementation of the Client class, which is responsible
+ * for handling socket communication. It allows sending and receiving messages
+ * over a specified port.
+ *
+ * @include <SFML/Network.hpp>
+ * @include <vector>
+ * @include <iostream>
+ */
+
 #include "../include/Client.hpp"
 #include <iostream>
 
+/**
+ * @brief Constructs a Client object and binds it to the specified port.
+ *
+ * This constructor attempts to bind the socket to the given port. If the binding
+ * fails, an error message is printed to the standard error stream, and the program
+ * exits with a status of 1.
+ *
+ * @param port The port number to which the socket will be bound.
+ */
 Client::Client(int port) {
     if (socket.bind(port) != sf::Socket::Done) {
-        std::cerr << "Erreur lors de la liaison du socket au port " << port << std::endl;
+        std::cerr << "Error binding socket to port " << port << std::endl;
         exit(1);
     }
-    std::cout << "Le client écoute sur le port " << port << "..." << std::endl;
-
+    std::cout << "Client listening on port " << port << "..." << std::endl;
 }
 
-
-
+/**
+ * @brief Sends a message to a specified recipient.
+ *
+ * This function sends a message to the specified recipient IP address and port.
+ * If the sending fails, an error message is printed to the standard error stream.
+ *
+ * @param message A vector of characters representing the message to be sent.
+ * @param recipient The IP address of the recipient.
+ * @param port The port number of the recipient.
+ */
 void Client::send(const std::vector<char>& message, const sf::IpAddress& recipient, unsigned short port) {
     if (socket.send(message.data(), message.size(), recipient, port) != sf::Socket::Done) {
-        std::cerr << "Erreur lors de l'envoi du message" << std::endl;
+        std::cerr << "Error sending message" << std::endl;
     }
 }
 
+/**
+ * @brief Receives a message from a sender.
+ *
+ * This function attempts to receive a message into a buffer. If the reception
+ * is successful, the received message is printed along with the sender's IP address.
+ * Otherwise, an error message is printed to the standard error stream.
+ */
 void Client::receive() {
     char buffer[128];
     std::size_t received;
@@ -25,9 +61,9 @@ void Client::receive() {
     unsigned short port;
 
     if (socket.receive(buffer, sizeof(buffer), received, sender, port) != sf::Socket::Done) {
-        std::cerr << "Erreur lors de la réception du message" << std::endl;
+        std::cerr << "Error receiving message" << std::endl;
     } else {
         std::vector<char> message(buffer, buffer + received);
-        std::cout << "Message reçu de " << sender << " : " << std::endl;
+        std::cout << "Message received from " << sender << " : " << std::endl;
     }
 }
