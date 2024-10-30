979db2d670f23a01998dbab3b90e32541f1db36f
matheo2027 <matheo.piques@epitech.eu>
2024-10-30 20:26:45 +0100

    [UPD] ADD doc SystemManager.hpp

--- ecs/include/SystemManager.hpp
+++ ecs/include/SystemManager.hpp
@@ -1,3 +1,15 @@
+/**
+ * @file SystemManager.hpp
+ * @brief Manages all systems in the ECS (Entity-Component-System) architecture.
+ *
+ * This file defines the SystemManager class, which is responsible for updating
+ * and managing different systems used in the game. It holds a collection of systems
+ * that implement the ISystem interface.
+ *
+ * @note Systems are used to handle specific game functionalities such as movement,
+ * rendering, player interactions, enemy behavior, bullet management, and box handling.
+ */
+
 #pragma once
 
 #include "systems/Movement.hpp"
@@ -7,19 +19,49 @@
 #include "systems/Bullet.hpp"
 #include "systems/Box.hpp"
 
-
 #include <vector>
 
 namespace ecs
 {
-
-class SystemManager
-{
+    /**
+     * @class SystemManager
+     * @brief Class responsible for managing and updating game systems.
+     *
+     * The SystemManager class maintains a collection of systems and updates them
+     * during each frame of the game. It allows for adding new systems dynamically
+     * at runtime.
+     */
+    class SystemManager
+    {
     public:
+        /**
+         * @brief Constructs a new SystemManager object.
+         *
+         * Initializes the SystemManager and prepares it to manage game systems.
+         */
         SystemManager();
 
+        /**
+         * @brief Updates all managed systems.
+         *
+         * This method is called each frame to update all systems with the
+         * elapsed time since the last frame.
+         *
+         * @param dt The elapsed time since the last frame, in seconds.
+         */
         void update(float dt);
 
+        /**
+         * @brief Adds a new system to the manager.
+         *
+         * This method allows for the addition of a new system of type T,
+         * which must inherit from systems::ISystem. The system is constructed
+         * with the provided arguments.
+         *
+         * @tparam T The type of system to add.
+         * @tparam Args Variadic template for constructor arguments of the system.
+         * @param args The constructor arguments for the new system.
+         */
         template <typename T, typename... Args>
         void addSystem(Args &&...args)
         {
@@ -27,8 +69,11 @@ class SystemManager
         }
 
     private:
+        /**
+         * @brief Vector of unique pointers to the systems managed by this class.
+         *
+         * This vector stores all systems that the SystemManager is responsible for.
+         */
         std::vector<std::unique_ptr<systems::ISystem>> systems;
-
-};
-
+    };
 }
