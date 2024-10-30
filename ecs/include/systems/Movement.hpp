1d7af1ca46936a5cb0108a97f198a4d1a9a12309
matheo2027 <matheo.piques@epitech.eu>
2024-10-30 20:23:01 +0100

    [UPD] ADD doc Movement.hpp

--- ecs/include/systems/Movement.hpp
+++ ecs/include/systems/Movement.hpp
@@ -1,3 +1,12 @@
+/**
+ * @file Movement.hpp
+ * @brief Movement system for managing entity movements.
+ *
+ * This file contains the declaration of the Movement class, which is
+ * responsible for updating the position of entities based on their
+ * velocity in the game.
+ */
+
 #pragma once
 
 #include <vector>
@@ -10,15 +19,31 @@
 namespace systems
 {
 
+/**
+ * @class Movement
+ * @brief A system for updating entity movements based on velocity.
+ *
+ * The Movement class implements the ISystem interface and is used to
+ * manage and update the movement of entities in the game. It modifies
+ * the position of entities based on their velocity each frame.
+ */
 class Movement : public ISystem
 {
     public:
+        /**
+         * @brief Constructs a Movement system.
+         * @param entityManager Reference to the EntityManager for managing entities.
+         */
         Movement(ecs::EntityManager &entityManager);
 
+        /**
+         * @brief Updates the positions of entities based on their velocity.
+         * @param dt The time elapsed since the last update, in seconds.
+         */
         void update(float dt);
 
     private:
-        ecs::EntityManager &m_entityManager;
+        ecs::EntityManager &m_entityManager; ///< Reference to the EntityManager.
 };
 
-}
+} // namespace systems
