fc746b38f9c2578cddb37569cd9c0ef23f9e64b6
matheo2027 <matheo.piques@epitech.eu>
2024-10-31 03:42:41 +0100

    [UPD] add the index to render the doc

--- build.bat
+++
@@ -1,2 +0,0 @@
-If not exist build\ (mkdir build)
-cd build && conan install .. --build=missing  -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True && cd .. && cmake -B ./build -DCMAKE_BUILD_TYPE=Release && cmake --build ./build -j && copy .\build\client\r-type_client.exe
