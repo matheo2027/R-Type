f4ca494c08d8341726681a86f3f19bbe818d1f82
matheo2027 <matheo.piques@epitech.eu>
2024-11-03 20:56:02 +0100

    [FIX] Compile linux is ok

--- CompileLinux.sh
+++ CompileLinux.sh
@@ -1,2 +1,8 @@
+# Supprimer le dossier de build existant
 rm -rf build
+
+# Download SFML
+sudo dnf install -y SFML-devel
+
+# Générer et compiler le projet avec CMake
 cmake -S . -B build && cmake --build build
