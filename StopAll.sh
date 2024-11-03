632a29104e1420204ef7cb292723476aed31f0d6
matheo2027 <matheo.piques@epitech.eu>
2024-11-03 20:40:13 +0100

    [UPD] Modifications of .sh

--- StopAll.sh
+++
@@ -1,5 +0,0 @@
-# Nettoie le cache CMake si nécessaire
-rm -rf CMakeCache.txt CMakeFiles Doxyfile *.cmake CMakeDoxyfile.in Makefile ecs/Makefile ecs/*.cmake ecs/CMakeFiles client/Makefile client/*.cmake client/CMakeFiles server/Makefile server/*.cmake server/CMakeFiles
-
-# Suppr le dossier build
-rm -rf build
