3d8dc1a9667a017519b57b5847ba2bfc382378af
matheo2027 <matheo.piques@epitech.eu>
2025-02-03 16:37:54 +0100

    [ADD] Third party library

--- CompileLinux.sh
+++ CompileLinux.sh
@@ -1,8 +1,5 @@
 # Supprimer le dossier de build existant
 rm -rf build
 
-# Download SFML
-sudo dnf install -y SFML-devel
-
 # Générer et compiler le projet avec CMake
-cmake -S . -B build && cmake --build build
+cmake -S . -B build  && cmake --build build
