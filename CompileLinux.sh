fc0681b206a4fa747747a265ba850d0cbf03992a
matheo2027 <matheo.piques@epitech.eu>
2025-02-06 12:24:54 +0100

    [UPD] compilation SFML

--- CompileLinux.sh
+++ CompileLinux.sh
@@ -1,5 +1,8 @@
 # Supprimer le dossier de build existant
 rm -rf build
 
+# Insatll dependences SFML
+sudo dnf install  openal-soft-devel libvorbis-devel libogg-devel flac-devel
+
 # Générer et compiler le projet avec CMake
 cmake -S . -B build  && cmake --build build
